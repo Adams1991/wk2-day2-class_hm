4211222bb18ad93c235544efadd48f038a2ca80b
Adams1991 <adams121@hotmail.co.uk>
2018-05-29 17:45:53 +0100

    bear_spec setup complete

---
+++ specs/bears_spec.rb
@@ -0,0 +1,15 @@
+require("minitest/autorun")
+require_relative("../river.rb")
+require_relative("../bears.rb")
+require_relative("../fish.rb")
+
+ class BearTest < MiniTest::Test
+
+  def setup
+    @bear = Bear.new("John", "Panda", [])
+  end
+
+
+
+
+end
